8861f25a8e314ee842977ef9c13f9c5a986255bc
meta-bhavika-mathur <41181792+meta-bhavika-mathur@users.noreply.github.com>
2018-07-20 05:19:49 -0700

    Update Area.java

--- Assignment3/Area.java
+++ Assignment3/Area.java
@@ -2,6 +2,8 @@ package assignment3;
 
 class Area
 {
+	final String error_message = "Inputs should be greater than 0";
+
 	/**
 	 * Find area of rectangle.
 	 * @param width, width of the rectangle requires that it should be always greater than 0
@@ -9,12 +11,13 @@ class Area
 	 * @return area, containing area of rectangle
 	 * @throws ArithmeticException
 	 */
-	public double areaOfRectangle(double width,double height) throws ArithmeticException
+	public double areaOfRectangle(double width , double height) throws ArithmeticException
 	{
 		if(width <= 0.0 || height <= 0.0)
-			throw new ArithmeticException("Inputs should be greater than 0");		
+			throw new ArithmeticException(error_message);		
 		return width * height;
 	}
+
 	/**
 	 * Find area of Square.
 	 * @param side, side of square requires that it should be greater than 0
@@ -24,9 +27,10 @@ class Area
 	public double areaOfSquare(double side) throws ArithmeticException
 	{
 		if(side <= 0.0)
-			throw new ArithmeticException("Input should be greater than 0");
+			throw new ArithmeticException(error_message);
 		return side * side;
 	}
+
 	/**
 	 * Find area of Circle.
 	 * @param radius, radius of circle requires that it should be greater than 0
@@ -36,9 +40,10 @@ class Area
 	public double areaOfCircle(double radius) throws ArithmeticException
 	{
 		if(radius <= 0.0)
-			throw new ArithmeticException("Input should be greater than 0");
+			throw new ArithmeticException(error_message);
 		return (22 * radius * radius) / 7 ;
 	}
+
 	/**
 	 * Find area of Triangle.
 	 * @param base, base of triangle requires that it should be greater than 0
@@ -49,7 +54,7 @@ class Area
 	public double areaOfTriangle(double base, double height) throws ArithmeticException
 	{
 		if(base <= 0.0 || height <=0.0)
-			throw new ArithmeticException("Inputs should be greater than 0");
+			throw new ArithmeticException(error_message);
 		return (base * height) / 2;
 	}
 }
