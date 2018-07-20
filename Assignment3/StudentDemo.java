4e7f6e4a99691f5baa79089697a28e9a6ce4bdac
meta-bhavika-mathur <41181792+meta-bhavika-mathur@users.noreply.github.com>
2018-07-20 05:23:44 -0700

    Update StudentDemo.java

--- Assignment3/StudentDemo.java
+++ Assignment3/StudentDemo.java
@@ -9,12 +9,17 @@ class StudentDemo
 		Marksheet first_sheet;
 		double grade;
 		int choice;
+
 		try
 		{
 			System.out.println("Enter number of students:");
 			Scanner in = new Scanner(System.in);
 			int num = in.nextInt();
 			first_sheet = new Marksheet(num);
+
+			Marksheet second_sheet = new Marksheet(num);
+			second_sheet.getStudentGrade(1);
+			
 			System.out.println("Choose an operation to perform:");
 			do
 			{
@@ -28,24 +33,30 @@ class StudentDemo
 				switch(choice)
 				{
 					case 1: for(int i=1; i<=num ; i++)
-							{
-							    System.out.println("Enter grade for student " + i + ":");
-								grade = in.nextDouble();
-								first_sheet.setGrade(i,grade);
-							}
-							break;
+					 	{
+							System.out.println("Enter grade for student " + i + ":");
+							grade = in.nextDouble();
+							first_sheet.setStudentGrade(i,grade);
+						}
+						break;
+
 					case 2: System.out.println("Enter student number:");
-						    num = in.nextInt();
-						    System.out.println("Grade of student" + num + ":" + first_sheet.getGrade(num));
-						    break;
-					case 3: System.out.println("Average grade:" + first_sheet.averageGrade());
-						    break;
+						num = in.nextInt();
+						System.out.println("Grade of student" + num + ":" + first_sheet.getStudentGrade(num));
+						break;
+
+					case 3: System.out.println("Average grade:" + first_sheet.averageOfGrades());
+					 	break;
+
 					case 4: System.out.println("Minimum grade:" + first_sheet.minimumGrade());
-						    break;
-				    case 5: System.out.println("Maximum grade:" + first_sheet.maximumGrade());
-						    break;
-				    case 6: System.out.println("Percentage of passed students:" + first_sheet.percentagePassed());
-						    break;
+						break;
+
+				    	case 5: System.out.println("Maximum grade:" + first_sheet.maximumGrade());
+						break;
+
+				    	case 6: System.out.println("Percentage of passed students:" + first_sheet.percentagePassed());
+						break;
+
 					default:System.out.println("Wrong choice!Try again");
 				}
 				System.out.println("Do you want to continue??:Type 1 for yes and 0 for NoList");
