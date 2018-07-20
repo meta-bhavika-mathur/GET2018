7de353bf461cb65495a85af0d820bbbdb092d2dd
meta-bhavika-mathur <41181792+meta-bhavika-mathur@users.noreply.github.com>
2018-07-20 05:23:01 -0700

    Update StringOperation.java

--- Assignment3/StringOperation.java
+++ Assignment3/StringOperation.java
@@ -9,16 +9,14 @@ class StringOperation
 	 */
 	public int getLength(String string)
 	{
-		String str = string ;
-		int length = 0 ;
-		int i = 0 ;
-		for(char ch:str.toCharArray())
+		int i = 0;
+		for(char ch:string.toCharArray())
 		{	
 			i++ ;
 		}
-		length = i ;
-		return length ;
+		return i;
 	}
+
 	/**
 	 * Find reverse of a string.
 	 * @param original, value to reverse requires that it should be not null and can be case-sensitive
@@ -26,47 +24,49 @@ class StringOperation
 	 */
 	public String reverseString(String original)
 	{
-		int len ;
-		String reverse = " ";
+		int len;
+		String reverse = "";
 		len = getLength(original);
-		for(int i=len-1 ; i>=0 ; i--)
+		for(int i=len-1 ; i>= 0 ; i--)
 		{
 			reverse += original.charAt(i);
 		}
-		return reverse ;
+		return reverse;
 	}
+
 	/**
 	 * Compare 2 strings.
 	 * @param first_string, first string to compare requires that it should be not null and can be case-sensitive
 	 * @param second_string, second string to compare requires that it should be not null and can be case-sensitive
 	 * @return result, true if first_string = second_string otherwise false
 	 */
-	public boolean compareStrings(String first_string , String second_string)
+	public int compareStrings(String first_string , String second_string)
 	{
-		boolean result = true;
+		int result = 1;
 		int length_of_first = getLength(first_string);
 		int length_of_second = getLength(second_string);
 		
 		if( length_of_first == length_of_second)
 		{
-			int i = 0 ;
+			int i = 0;
 			while( i <= length_of_first - 1)
 			{
 				if( first_string.charAt(i) == second_string.charAt(i))
 				{
-					i++ ;
+					i++;
 				}
 				else
 				{
-					result = false;
+					result = 0;
 					break;	
 				}
 			}
 		}
 		else
-			result = false;
+			result = 0;
 		return result;
 	}
+
 	/**
 	 * Replaces upper case characters with lower case and vice-versa of a string.
 	 * @param str, value to replace case requires that it should be not null and can be case-sensitive
@@ -74,9 +74,8 @@ class StringOperation
 	 */
 	public String replaceCase(String str)
 	{
-		String string = str;
-		String new_str = " ";
-		for(char ch:string.toCharArray())
+		String new_str = "";
+		for(char ch:str.toCharArray())
 		{	
 			if (ch >= 'A' && ch <= 'Z')
 				ch = (char) (ch + 32);
@@ -87,24 +86,24 @@ class StringOperation
 		}
 		return new_str;
 	}
+
 	/**
 	 * Find largest word of a string.
 	 * @param str, string to search for largest word, requires that it should be not null and can be case-sensitive
 	 * @return largest requires to be the largest from the last if 2 have equal length
 	 */
-    public String largestWord( String str ) throws NullPointerException
+    	public String largestWord( String str ) throws NullPointerException
 	{
-		String string = str;
-		String max_string = " ";
+		String max_string = "";
 		int max_length = 0;
 		int count = 0;
 		int lwr = 0;
 		int upr = 0;
 		int flag = 0;
-		for(int i = 0 ; i <= getLength(string) - 1; i++)
+		for(int i = 0 ; i <= getLength(str) - 1; i++)
 		{
-		    System.out.println("length" + getLength(string));
-			if(( string.charAt(i) == ' ') || ( string.charAt(i) == '\n') || ( string.charAt(i) == '\t'))
+		    System.out.println("length" + getLength(str));
+			if(( str.charAt(i) == ' ') || ( str.charAt(i) == '\n') || ( str.charAt(i) == '\t'))
 			{
 				flag = 0;
 				if(count > max_length)
@@ -119,7 +118,7 @@ class StringOperation
 			    flag = 1;
 			    count++;
 			    if(count > max_length)
-                {
+                		{
 			        lwr = i;
                 }
 			}
@@ -131,7 +130,7 @@ class StringOperation
 		for(int j = lwr; j <= upr; j++)
 		{
 		    System.out.println("j:" + j);
-			max_string += string.charAt(j);
+			max_string += str.charAt(j);
 		}
 		return max_string;
 	}
