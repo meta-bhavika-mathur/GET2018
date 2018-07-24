2dd8486a0a1c8f391210d865f2cfae4a715f7ccd
meta-bhavika-mathur <41181792+meta-bhavika-mathur@users.noreply.github.com>
2018-07-23 22:03:46 -0700

    Update MathOperationTest.java

--- Assignment4-Recursion/MathOperationTest.java
+++ Assignment4-Recursion/MathOperationTest.java
@@ -10,22 +10,22 @@ public class MathOperationTest {
     //Positive test cases for LCM
     @Test
     public void lcmTest1() {
-        assertEquals(24, obj.leastCommonMultiple(8,3));
+        assertEquals(24, obj.findLeastCommonMultiple(8,3));
     }
 
     @Test
     public void lcmTest2() {
-        assertEquals(30, obj.leastCommonMultiple(15,10));
+        assertEquals(30, obj.findLeastCommonMultiple(15,10));
     }
 
     //Negative test cases for LCM
     @Test
     public void lcmTest3() {
-        assertNotEquals(14, obj.leastCommonMultiple(4,2));
+        assertNotEquals(14, obj.findLeastCommonMultiple(4,2));
     }
     @Test
     public void lcmTest4() {
-        assertNotEquals(8, obj.leastCommonMultiple(15,10));
+        assertNotEquals(8, obj.findLeastCommonMultiple(15,10));
     }
     
     @Test (expected = AssertionError.class)
@@ -36,7 +36,7 @@ public class MathOperationTest {
     //When first argument is less than second
     @Test (expected = AssertionError.class)
     public void lcmFirstNumSmall1() {
-        assertEquals(4, obj.leastCommonMultiple(2,4));
+        assertEquals(4, obj.findLeastCommonMultiple(2,4));
     }
     
     //When an argument is less than second
@@ -49,35 +49,35 @@ public class MathOperationTest {
     //Positive test cases for HCF
     @Test
     public void hcfTest1() {
-        assertEquals(5, obj.highestCommonFactor(15, 10));
+        assertEquals(5, obj.findHighestCommonFactor(15, 10));
     }
 
     @Test
     public void hcfTest2() {
-        assertEquals(2, obj.highestCommonFactor(4, 2));
+        assertEquals(2, obj.findHighestCommonFactor(4, 2));
     }
     
     //Negative test cases for HCF
     @Test
     public void hcfTest3() {
-        assertNotEquals(15, obj.highestCommonFactor(15, 10));
+        assertNotEquals(15, obj.findHighestCommonFactor(15, 10));
     }
 
     @Test
     public void hcfTest4() {
-        assertNotEquals(3, obj.highestCommonFactor(6, 2));
+        assertNotEquals(3, obj.findHighestCommonFactor(6, 2));
     }
     
     //When first argument is less than second
     @Test (expected = AssertionError.class)
     public void hcfFirstNumSmall1() {
-        assertEquals(2, obj.highestCommonFactor(2,4));
+        assertEquals(2, obj.findHighestCommonFactor(2,4));
     }
     
     //When an argument is less than second
     @Test (expected = AssertionError.class)
     public void hcfNumNegative() {
-        assertEquals(2, obj.highestCommonFactor(-2,4));
+        assertEquals(2, obj.findHighestCommonFactor(-2,4));
     }
     
 }   
