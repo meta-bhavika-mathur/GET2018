63bb67bd3dd548f260aef4ab8612492a14fb76f6
meta-bhavika-mathur <41181792+meta-bhavika-mathur@users.noreply.github.com>
2018-07-26 09:09:53 +0530

    Update Polynomial.java

--- Assignment5/Polynomial.java
+++ Assignment5/Polynomial.java
@@ -3,51 +3,16 @@ package assignment5;
 public final class Polynomial
 {
     private int[][] polynomial_array;
+    final private String NULL_POINTER_EXCEPTION_MESSAGE = "Null pointer exception!";
+    final private String OUT_OF_BOUNDS_MESSSAGE = "Array index out of bounds exception!";
     
-    /** Utility method to convert 1D Array into 2D Array, whose first column represent coefficient and 
-     	their powers.
-     * @param one_dimensional_array, 1D Array with coefficient stored at index equal to exponent.
-     * @return 2D array with coefficient and exponent stored, requires that coefficient must not be zero.
-     */
-    private int[][] convertIntoTwoDimensional(int[] one_dimensional_array)
-    {
-        int polynomial_size = 0;
-        
-        for( int i = 0; i < one_dimensional_array.length; i++ )
-        {
-            if( one_dimensional_array[i] != 0 )
-            	polynomial_size++;
-        }
-        
-        int[][] new_array = new int[polynomial_size][2];
-        int count = 0;
-        
-        for(int i=0; i < one_dimensional_array.length; i++)
-        {
-            if(one_dimensional_array[i] != 0)
-            {
-            	new_array[count][0] = one_dimensional_array[i];
-            	new_array[count][1] = i;
-            	count++;
-            }
-        }
-        return new_array;
-    }
-    
-   
     // Parameterized constructor of  class Polynomial.
-    Polynomial(int[] array)
+    Polynomial(int[] array) throws NullPointerException
     {
-        polynomial_array = convertIntoTwoDimensional(array); 
-    }
-    
-    /**
-     * To get 2D array of polynomial with stored coefficients and their powers
-     * @return polynomial in the form of 2D array.
-     */
-    int[][] getPolynomialArray()
-    {
-        return polynomial_array;
+    	 if( array.length <= 0)
+         	throw new NullPointerException(NULL_POINTER_EXCEPTION_MESSAGE);
+    	 
+    	polynomial_array = convertIntoTwoDimensional(array); 
     }
     
     /**
@@ -55,24 +20,27 @@ public final class Polynomial
      * @param variable, value for variable of polynomial to evaluate the polynomial expression.
      * @return result, the result obtained after evaluating polynomial expression for given variable.
      */
-    float evaluatePolynomial(int variable)
+    public float evaluatePolynomial(int variable) throws NullPointerException
     {
-        float result = 0;
-        
-        for(int i = 0; i < polynomial_array.length; i++)
-        	result += polynomial_array[i][0] * (Math.pow(variable, polynomial_array[i][1]));           
+    	 if( polynomial_array.length <= 0)
+          	throw new NullPointerException(NULL_POINTER_EXCEPTION_MESSAGE);
+    	
+    	 float result = 0;
+  
+         for(int i = 0; i < polynomial_array.length; i++)
+        	 result += polynomial_array[i][0] * (Math.pow(variable, polynomial_array[i][1]));           
     
-        return result;
+         return result;
     }
     
     /**
      * To return degree of polynomial i.e the highest value of exponent in the expression.
      * @return highest power in the polynomial.
      */
-    int getPolynomialDegree() throws IndexOutOfBoundsException
+    public int getPolynomialDegree() throws IndexOutOfBoundsException
     {
         if( polynomial_array.length == 0)
-            throw new IndexOutOfBoundsException();
+            throw new IndexOutOfBoundsException(OUT_OF_BOUNDS_MESSSAGE);
         
         return polynomial_array[ polynomial_array.length - 1  ][ 1 ];
     }
@@ -83,28 +51,33 @@ public final class Polynomial
      * @param second, first polynomial as operand for addition operation
      * @return, a polynomial expressions representing sum of given 2 polynomial expressions.
      */
-    Polynomial addPolynomial(Polynomial first, Polynomial second)
+    public Polynomial addPolynomials(Polynomial polynomial) 
     {
-        int[] sum;
-        int max;
-        
-        // Find polynomial with highest degree
-        if( first.getPolynomialDegree() >= second.getPolynomialDegree() )
-        	max = first.getPolynomialDegree();	
-        else
-        	max = first.getPolynomialDegree();
-        
-        sum = new int[ max + 1 ];
+    	if( polynomial_array.length <= 0)
+          	throw new NullPointerException(NULL_POINTER_EXCEPTION_MESSAGE);
     	
+    	int[] sum;
+    	
+        // Compare which polynomial has higher degree to initialize new matrix of that size
+        if(getPolynomialDegree() >= polynomial.getPolynomialDegree() )
+        {
+        	sum = new int[getPolynomialDegree() + 1];
+        }
+        else
+        {
+        	sum = new int[polynomial.getPolynomialDegree() + 1];
+        }
+        
+        // Copying all the elements of bigger polynomial array into new polynomial array
         for(int i = 0; i < polynomial_array.length; i++)
         {
-        	sum[ polynomial_array[i][1] ] = polynomial_array[i][0];
+        	sum[polynomial_array[i][1]] = polynomial_array[i][0];
         }
-   
-        for(int i=0; i < second.polynomial_array.length; i++)
-        {
-        	sum[ second.polynomial_array[i][1] ] += second.polynomial_array[i][0];
-        }
+        
+        // Performing sum
+        for(int i = 0; i < polynomial.polynomial_array.length; i++)
+        	sum[polynomial.polynomial_array[i][1]] += polynomial.polynomial_array[i][0];
+       
         return new Polynomial(sum);
     }
     
@@ -114,17 +87,67 @@ public final class Polynomial
      * @param second, second polynomial as operand for multiplication.
      * @return result of multiplication of two polynomial expressions
      */
-    Polynomial multiplyPolynomial(Polynomial first, Polynomial second)
+    public Polynomial multiplyPolynomials(Polynomial polynomial) throws NullPointerException
     {
-        int[] product_array = new int[ first.getPolynomialDegree() + second.getPolynomialDegree() + 1 ];
-        for(int i = 0; i < first.getPolynomialDegree(); i++)
+    	if(( polynomial_array.length <= 0) || (polynomial.polynomial_array.length <= 0))
+    		throw new NullPointerException(NULL_POINTER_EXCEPTION_MESSAGE);
+    	
+    	int[] product_array = new int[ getPolynomialDegree() + polynomial.getPolynomialDegree() + 1 ];
+    	
+    	// Perform multiplication
+        for(int i = 0; i < getPolynomialDegree(); i++)
         {
-            for(int j=0; j < second.getPolynomialDegree(); j++)
+            for(int j=0; j < polynomial.getPolynomialDegree(); j++)
             {
-            	product_array[first.polynomial_array[i][1] + second.polynomial_array[j][1]] += 
-            			first.polynomial_array[i][0] * second.polynomial_array[j][0];
+            	product_array[polynomial_array[i][1] + polynomial.polynomial_array[j][1]] += 
+            			polynomial_array[i][0] * polynomial.polynomial_array[j][0];
             }
         }
         return  new Polynomial(product_array);
     }
+    
+    /** Utility method to convert 1D Array into 2D Array, whose first column represent coefficient and 
+ 		their powers.
+     * @param one_dimensional_array, 1D Array with coefficient stored at index equal to exponent.
+     * @return 2D array with coefficient and exponent stored, requires that coefficient must not be zero.
+     */
+    private int[][] convertIntoTwoDimensional(int[] one_dimensional_array) throws NullPointerException
+    {
+    	int polynomial_size = 0;
+    	
+    	if( one_dimensional_array.length <= 0)
+    		throw new NullPointerException(NULL_POINTER_EXCEPTION_MESSAGE);
+    
+    	for( int i = 0; i < one_dimensional_array.length; i++ )
+    	{
+    		if( one_dimensional_array[i] != 0 )
+    			polynomial_size++;
+    	}
+    
+    	int[][] new_array = new int[polynomial_size][2];
+    	int count = 0;
+    
+    	for(int i=0; i < one_dimensional_array.length; i++)
+    	{
+    		if(one_dimensional_array[i] != 0)
+    		{
+    			new_array[count][0] = one_dimensional_array[i];
+    			new_array[count][1] = i;
+    			count++;
+    		}
+    	}
+    	return new_array;
+    }
+
+    /**
+     * Utility method to get 2D array of polynomial with stored coefficients and their powers
+     * @return polynomial in the form of 2D array.
+     */
+    int[][] getPolynomialArray() throws NullPointerException
+    {
+    	if( polynomial_array.length <= 0)
+    		throw new NullPointerException(NULL_POINTER_EXCEPTION_MESSAGE);
+    	
+    	return polynomial_array;
+    }    
 }
