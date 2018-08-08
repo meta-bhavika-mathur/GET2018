6ea46dfdd1a88bef08eeb38f6990e9eda253dbfc
meta-bhavika-mathur <41181792+meta-bhavika-mathur@users.noreply.github.com>
2018-08-08 22:13:43 +0530

    Add files via upload

---
+++ EmployeeComparision.java
@@ -0,0 +1,47 @@
+package employeesorting;
+
+import java.util.*;
+
+public class EmployeeComparision 
+{
+	List<Employee> listOfEmployees;
+	
+	// Constructor for EmployeeComparision class
+	public EmployeeComparision()
+	{
+		listOfEmployees = new ArrayList<Employee>();
+	}
+	
+	/**
+	 * To add new employee to the list of employees requires that employee id should be unique
+	 * @param employee, employee to insert into the employee list.
+	 * @return true, if the employee is successfully added, otherwise false.
+	 */
+	public boolean addNewEmployee(Employee employee)
+	{
+		for(Employee emp : listOfEmployees)
+		{
+			if(emp.getEmployeeId() == employee.getEmployeeId())
+				return false;
+		}
+		listOfEmployees.add(employee);
+		return true;
+	}
+	
+	/**
+	 * To get list of employees
+	 */
+	public List<Employee> getEmployeeList()
+	{
+		return listOfEmployees;
+	}
+	
+	/**
+	 * To sort list on the basis of name in alphabetical order
+	 */
+	public List<Employee> sortListNames()
+	{
+		Collections.sort(listOfEmployees, new SortByName());
+		return listOfEmployees;
+	}
+}
---
+++ TestEmployeeNameBasedSorting.java
@@ -0,0 +1,95 @@
+package employeesorting;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+import employee.Employee;
+import employee.EmployeeComparision;
+
+import java.util.*;
+
+public class TestEmployeeNameBasedSorting 
+{
+	EmployeeComparision employee = new EmployeeComparision();
+	
+	/**
+	 * Test to add a new employee to the employee list
+	 */
+	@Test
+    public void testAddEmployee()
+    {
+        boolean expectedOutput = true;
+        boolean actualOutput = employee.addNewEmployee(new Employee(7, "ABC", "Address"));
+        assertEquals(expectedOutput, actualOutput);
+    }
+	
+	/**
+	 * Test to add a new employee to the employee list with id equal to another existing employee
+	 */
+	@Test
+    public void testAddEmployeeWithAlreadyExistingId()
+    {
+		employee.addNewEmployee(new Employee(7, "DDC", "Address"));
+        boolean actualOutput = employee.addNewEmployee(new Employee(7, "ABC", "Address"));
+        boolean expectedOutput = false;
+        assertEquals(expectedOutput, actualOutput);
+    }
+	
+	/** 
+	 *To test natural order sorting of objects of employee class based on their id
+	 */
+	@Test
+	public void testNaturalOrderSortingOnEmployeeId() 
+	{
+		
+		List<Employee> actualEmployeesList = new ArrayList<Employee>();
+		List<Employee> expectedEmployeesList = new ArrayList<Employee>();
+		 
+		employee.addNewEmployee(new Employee(7, "ABC", "Mansarovar"));
+		employee.addNewEmployee(new Employee(15, "ABB", "Malviya Nagar"));
+		employee.addNewEmployee(new Employee(1, "DEF", "Sitapura"));
+		employee.addNewEmployee(new Employee(4, "EFG", "Sanganer"));	
+		
+		Collections.sort(employee.getEmployeeList()); // Sorting based on natural ordering
+		actualEmployeesList = employee.getEmployeeList();
+		
+		expectedEmployeesList.add(new Employee(1, "DEF", "Sitapura"));
+		expectedEmployeesList.add(new Employee(4, "EFG", "Sanganer"));
+		expectedEmployeesList.add(new Employee(7, "ABC", "Mansarovar"));
+		expectedEmployeesList.add(new Employee(15, "ABB", "Malviya Nagar"));
+		
+		for(int i = 0; i < actualEmployeesList.size(); i++)
+			assertEquals(expectedEmployeesList.get(i).getEmployeeId(), actualEmployeesList.get(i).getEmployeeId());		
+	}
+	
+	/**
+	 * Test to sort employee list on the basis of names 
+	 */
+	@Test
+	public void testSortingOnEmployeeName() 
+	{
+		List<Employee> actualEmployeesList = new ArrayList<Employee>();
+		List<Employee> expectedEmployeesList = new ArrayList<Employee>();
+		 
+		employee.addNewEmployee(new Employee(7, "ABC", "Mansarovar"));
+		employee.addNewEmployee(new Employee(15, "ABB", "Malviya Nagar"));
+		employee.addNewEmployee(new Employee(1, "DEF", "Sitapura"));
+		employee.addNewEmployee(new Employee(4, "EFG", "Sanganer"));		
+		
+		
+		actualEmployeesList = employee.sortListNames();
+		
+		expectedEmployeesList.add(new Employee(15, "ABB", "Malviya Nagar"));
+		expectedEmployeesList.add(new Employee(7, "ABC", "Mansarovar"));
+		expectedEmployeesList.add(new Employee(1, "DEF", "Sitapura"));
+		expectedEmployeesList.add(new Employee(4, "EFG", "Sanganer"));
+		
+		for(int i = 0; i < actualEmployeesList.size(); i++)
+			assertEquals(expectedEmployeesList.get(i).getEmployeeName(), actualEmployeesList.get(i).getEmployeeName());		
+	}	
+}
