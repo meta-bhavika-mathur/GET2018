32b8b02e33046772f7cab782f5fc945d3d1d2a5a
meta-bhavika-mathur <41181792+meta-bhavika-mathur@users.noreply.github.com>
2018-08-10 08:21:22 +0530

    Update StackInterface.java

--- StackInterface.java
+++ StackInterface.java
@@ -4,7 +4,7 @@ package Session1;
 public interface StackInterface<T>
 {  
     public boolean push(T data);
-    public T pop();
+    public T pop()  throws UnderflowException;
     public boolean isEmpty();
-    public T top();
+    public T top() throws UnderflowException;
 }
