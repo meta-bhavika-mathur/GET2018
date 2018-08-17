9207b40ac0b965b4ecefdaad32fe0aad63f74901
meta-bhavika-mathur <41181792+meta-bhavika-mathur@users.noreply.github.com>
2018-08-17 04:31:17 -0700

    Update createtables.sql

--- createtables.sql
+++ createtables.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE StoreFront;
 USE StoreFront;
 
-CREATE TABLE User         ( user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+ CREATE TABLE User         (user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
                             email_id VARCHAR(100) NOT NULL UNIQUE, 
                             password VARCHAR(15) NOT NULL, 
                             first_name VARCHAR(100) NOT NULL,
                             last_name VARCHAR(100),
-                            user_type VARCHAR(15) CHECK(user_type IN(Administrator, Shopper)) );
+                            user_type VARCHAR(15) CHECK (user_type = 'Administrator' OR user_type = 'Shopper') );
                     
 CREATE TABLE PhoneNumber  ( user_id INT NOT NULL,
                             phone_number INT NOT NULL,
@@ -35,9 +35,8 @@ CREATE TABLE Orders      (  order_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
                             FOREIGN KEY (user_id)
                             REFERENCES User(user_id) ON UPDATE CASCADE 
                             ON DELETE RESTRICT);
-
-
-CREATE TABLE Product    (   product_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                            
+ CREATE TABLE Product    (  product_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
                             product_name VARCHAR(50),
                             product_price DOUBLE,
                             product_description TEXT(255),
@@ -56,13 +55,13 @@ CREATE TABLE Product    (   product_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
                                  ON DELETE RESTRICT,
                                  PRIMARY KEY (order_id, product_id));                           
                      
-CREATE TABLE Category   (   category_id INT NOT NULL PRIMARY KEY,
-                            parent_category_id INT NOT NULL,
+CREATE TABLE Category   (   category_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                            parent_category_id INT,
                             category_name VARCHAR(50),
                             FOREIGN KEY(parent_category_id) REFERENCES Category(category_id) ON 
                             UPDATE CASCADE ON DELETE RESTRICT);
-
-CREATE TABLE ProductCategory (  product_id INT NOT NULL,
+                            
+ CREATE TABLE ProductCategory ( product_id INT NOT NULL,
                                 category_id INT NOT NULL,
                                 FOREIGN KEY(product_id) REFERENCES Product(product_id) ON 
                                 UPDATE CASCADE ON DELETE RESTRICT,
@@ -75,4 +74,3 @@ CREATE TABLE Image  (   image_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
                         alternative_text VARCHAR(50),
                         FOREIGN KEY(product_id) REFERENCES Product(product_id) ON 
                         UPDATE CASCADE ON DELETE RESTRICT);
-                   
