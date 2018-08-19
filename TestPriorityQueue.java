cc5deef2f8f09503ed9079d7341181c47117a6cc
meta-bhavika-mathur <41181792+meta-bhavika-mathur@users.noreply.github.com>
2018-08-19 20:10:27 +0530

    Update TestPriorityQueue.java

--- TestPriorityQueue.java
+++ TestPriorityQueue.java
@@ -1,116 +1,145 @@
-package priorityqueue;
+package numberofbowlers;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import java.util.*;
 
-public class TestPriorityQueue 
+public class TestFindOrderOfBowlers
 {
-	PriorityQueue newPriorityQueue = new PriorityQueue(10);
-	
-	/**
-	 * To test insertion of an element in a priority queue
-	 */
-	@Test
-	public void testInsertionInPriorityQueue() 
-	{
-		boolean firstResult = newPriorityQueue.insertElement("ABC", 2);
-		boolean secondResult = newPriorityQueue.insertElement("DEF", 10);
-
-		assertEquals(true, firstResult);
-		assertEquals(true, secondResult);		
-	}
-	
-	/**
-	 * To test removal of an element from a queue
-	 */
-	@Test
-	public void tesDeletionInPriorityQueue() 
-	{
-		newPriorityQueue.insertElement("ABC", 2);
-		newPriorityQueue.insertElement("DEF", 10);
-		newPriorityQueue.insertElement("GHI", 2);
-		newPriorityQueue.insertElement("JKL", 4);
-		newPriorityQueue.insertElement("MNO", 6);
-		
-		Node firstResult = newPriorityQueue.removeElement();
-		Node secondResult = newPriorityQueue.removeElement();
-		
-		String firstResultValue = firstResult.getData();
-		String secondResultValue = secondResult.getData();
-		String firstExpectedValue = "DEF";
-		String secondExpectedValue = "MNO";
-		
-		assertEquals(firstExpectedValue, firstResultValue);
-		assertEquals(secondExpectedValue, secondResultValue);		
-	}
-	
-	/**
-	 * Test to get element with maximum priority from the queue
-	 */
-	@Test
-	public void testToGetElementWithMaximumPriority() 
-	{
-		newPriorityQueue.insertElement("ABC", 2);
-		newPriorityQueue.insertElement("DEF", 10);
-		newPriorityQueue.insertElement("GHI", 2);
-		newPriorityQueue.insertElement("JKL", 4);
-		newPriorityQueue.insertElement("MNO", 6);
-		
-		Node result = newPriorityQueue.getElementWithMaximumPriority();	
-		String resultValue = result.getData();
-		String expectedValue = "DEF";
-		
-		assertEquals(expectedValue, resultValue);			
-	}
-	
-	/**
-	 * Test to get element with maximum priority from the queue when all elements have equal priority
-	 */
-	@Test
-	public void testToGetElementWithMaximumPriorityWhenAllPrioritiesAreEqual() 
-	{
-		newPriorityQueue.insertElement("ABC", 2);
-		newPriorityQueue.insertElement("DEF", 2);
-		newPriorityQueue.insertElement("GHI", 2);
-		newPriorityQueue.insertElement("JKL", 2);
-		newPriorityQueue.insertElement("MNO", 2);
-		
-		Node result = newPriorityQueue.getElementWithMaximumPriority();	
-		String resultValue = result.getData();
-		String expectedValue = "ABC";
-		
-		assertEquals(expectedValue, resultValue);			
-	}
-	
-	/**
-	 * Test to check method to check whether the queue is full or not
-	 */
-	@Test
-	public void testToCheckIsQueueFull() 
-	{
-		PriorityQueue newPriorityQueue = new PriorityQueue(5);
-		newPriorityQueue.insertElement("ABC", 2);
-		newPriorityQueue.insertElement("DEF", 10);
-		newPriorityQueue.insertElement("GHI", 2);
-		newPriorityQueue.insertElement("JKL", 4);
-		newPriorityQueue.insertElement("MNO", 6);
-		
-		boolean result = newPriorityQueue.isFull();	
-		boolean expectedValue = true;
-		
-		assertEquals(expectedValue, result);			
-	}
-	
-	/**
-	 * Test to check method to check whether the queue is empty or not
-	 */
-	@Test
-	public void testToCheckIsQueueEmpty() 
-	{
-		PriorityQueue newPriorityQueue = new PriorityQueue(5);
-		boolean result = newPriorityQueue.isEmpty();	
-		boolean expectedValue = true;
-		
-		assertEquals(expectedValue, result);			
-	}
+    /**
+     * To test the order of bowlers such that runs scored by opponent are minimum
+     */
+    @Test
+    public void testResult()
+    {
+        Node[] listOfBowlers = new Node[4];
+        FindOrderOfBowlers findOrder = new FindOrderOfBowlers();
+        
+        Node firstBowler = new Node("A", 7);
+        Node secondBowler = new Node("B", 3);
+        Node thirdBowler = new Node("C", 4);
+        Node fourthBowler = new Node("D", 1);
+        
+        listOfBowlers[0] = firstBowler;
+        listOfBowlers[1] = secondBowler;
+        listOfBowlers[2] = thirdBowler;
+        listOfBowlers[3] = fourthBowler;
+        
+        int numberOfBowlers = 4;
+        int totalBalls = 15;
+        
+        List<String> namesInOrderActualList = findOrder.findOrderOfBowling(numberOfBowlers, totalBalls, listOfBowlers);
+        List<String> expectedListOfOrder = new ArrayList<String>(totalBalls);
+        
+        expectedListOfOrder.add("A");   
+        expectedListOfOrder.add("A");     
+        expectedListOfOrder.add("A");     
+        expectedListOfOrder.add("A");     
+        expectedListOfOrder.add("C");     
+        expectedListOfOrder.add("C");     
+        expectedListOfOrder.add("B");     
+        expectedListOfOrder.add("A");     
+        expectedListOfOrder.add("A");     
+        expectedListOfOrder.add("C");     
+        expectedListOfOrder.add("B");     
+        expectedListOfOrder.add("B");     
+        expectedListOfOrder.add("A");     
+        expectedListOfOrder.add("C");     
+        expectedListOfOrder.add("C");     
+        
+        assertEquals(expectedListOfOrder, namesInOrderActualList);    
+    }
+    
+    /**
+     * To test the order of bowlers such that runs scored by opponent are minimum with number of 
+     * bowlers less than or equal to zero
+     */
+    @Test
+    public void testResultWithNumberOfBowlersLessThanOne()
+    {
+    	Node[] listOfBowlers = new Node[4];
+    	FindOrderOfBowlers findOrder = new FindOrderOfBowlers();
+        
+        Node firstBowler = new Node("A", 7);
+        Node secondBowler = new Node("B", 3);
+        Node thirdBowler = new Node("C", 4);
+        Node fourthBowler = new Node("D", 1);
+        
+        listOfBowlers[0] = firstBowler;
+        listOfBowlers[1] = secondBowler;
+        listOfBowlers[2] = thirdBowler;
+        listOfBowlers[3] = fourthBowler;
+        
+        int numberOfBowlers = 0;
+        int totalBalls = 15;
+        
+        List<String> namesInOrderActualList = findOrder.findOrderOfBowling(numberOfBowlers, totalBalls, listOfBowlers);
+        List<String> expectedListOfOrder = new ArrayList<String>(totalBalls); 
+        String ERROR_MESSAGE = "Invalid Input!";
+        expectedListOfOrder.add(ERROR_MESSAGE);
+        
+        assertEquals(expectedListOfOrder.get(0), namesInOrderActualList.get(0));    
+    }
+    
+    /**
+     * To test the order of bowlers such that runs scored by opponent are minimum with number of 
+     * balls less than or equal to zero
+     */
+    @Test
+    public void testResultWithNumberOfBallsLessThanOne()
+    {
+    	Node[] listOfBowlers = new Node[4];
+    	FindOrderOfBowlers findOrder = new FindOrderOfBowlers();
+        
+        Node firstBowler = new Node("A", 7);
+        Node secondBowler = new Node("B", 3);
+        Node thirdBowler = new Node("C", 4);
+        Node fourthBowler = new Node("D", 1);
+        
+        listOfBowlers[0] = firstBowler;
+        listOfBowlers[1] = secondBowler;
+        listOfBowlers[2] = thirdBowler;
+        listOfBowlers[3] = fourthBowler;
+        
+        int numberOfBowlers = 4;
+        int totalBalls = 0;
+        
+        List<String> namesInOrderActualList = findOrder.findOrderOfBowling(numberOfBowlers, totalBalls, listOfBowlers);
+        List<String> expectedListOfOrder = new ArrayList<String>(totalBalls); 
+        String ERROR_MESSAGE = "Invalid Input!";
+        expectedListOfOrder.add(ERROR_MESSAGE);
+        
+        assertEquals(expectedListOfOrder.get(0), namesInOrderActualList.get(0));    
+    }
+    
+    /**
+     * To test the order of bowlers such that runs scored by opponent are minimum with number of 
+     * balls less than the number of bowlers
+     */
+    @Test
+    public void testResultWithNumberOfBallsLessThanNumberOfBowlers()
+    {
+    	Node[] listOfBowlers = new Node[4];
+    	FindOrderOfBowlers findOrder = new FindOrderOfBowlers();
+        
+        Node firstBowler = new Node("A", 7);
+        Node secondBowler = new Node("B", 3);
+        Node thirdBowler = new Node("C", 4);
+        Node fourthBowler = new Node("D", 1);
+        
+        listOfBowlers[0] = firstBowler;
+        listOfBowlers[1] = secondBowler;
+        listOfBowlers[2] = thirdBowler;
+        listOfBowlers[3] = fourthBowler;
+        
+        int numberOfBowlers = 4;
+        int totalBalls = 2;
+        
+        List<String> namesInOrderActualList = findOrder.findOrderOfBowling(numberOfBowlers, totalBalls, listOfBowlers);
+        List<String> expectedListOfOrder = new ArrayList<String>(totalBalls); 
+        String ERROR_MESSAGE = "Invalid Input!";
+        expectedListOfOrder.add(ERROR_MESSAGE);
+        
+        assertEquals(expectedListOfOrder.get(0), namesInOrderActualList.get(0));    
+    }
 }
