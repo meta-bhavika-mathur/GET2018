2b7131bf960e3146aff4fc1440b19797dcbc8ce9
meta-bhavika-mathur <41181792+meta-bhavika-mathur@users.noreply.github.com>
2018-08-20 03:06:50 -0700

    Update Assignment3Part2.sql

--- Assignment3Part2.sql
+++ Assignment3Part2.sql
@@ -6,7 +6,7 @@ SELECT
     COUNT(pc.product_id) AS count_of_categories
 FROM
     Product AS p
-        LEFT JOIN
+LEFT JOIN
     ProductCategory AS pc ON p.product_id = pc.product_id
 GROUP BY 
     pc.product_id
@@ -15,16 +15,3 @@ HAVING
 
 # To display Count of products as per given price range..
 
-SELECT 
-    p.product_price AS Price_in_Rupees,
-    COUNT(*) AS Product_Count
-FROM
-    (SELECT 
-        CASE
-                WHEN p.product_price >= 0 AND p.product_price <= 100 THEN '0-100'
-                WHEN p.product_price > 100 AND p.product_price <= 500 THEN '100-500'
-                ELSE 'Above 500'
-            END AS product_price
-    FROM
-        product) pr
-GROUP BY p.product_price;
