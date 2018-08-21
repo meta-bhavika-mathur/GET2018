ee7a8f48ca5a0f6cb05a0571e82c79638f11bf98
meta-bhavika-mathur <41181792+meta-bhavika-mathur@users.noreply.github.com>
2018-08-20 21:49:11 -0700

    Update Assignment1Function2.sql

--- Assignment1Function2.sql
+++ Assignment1Function2.sql
@@ -3,21 +3,23 @@
 -- --------------------------------------------------------------------------------
 DELIMITER $$
 
-CREATE DEFINER =`root`@`localhost` FUNCTION `MonthWithMaximumOrders`(year YEAR) RETURNS int(11)
+CREATE DEFINER=`root`@`localhost` FUNCTION `MonthWithMaximumOrders`(year YEAR) RETURNS int(11)
     DETERMINISTIC
 BEGIN
-    DECLARE max INT;
-
+    DECLARE max INT(11);
+    
     SELECT month INTO max 
-    FROM
-        (
+    FROM (
             SELECT 
-            COUNT(order_id) AS count_order, MONTH(order_date) AS month
-            FROM orderproduct
-            WHERE YEAR(order_date) = year
-            GROUP BY MONTH(order_date)
-        ) op
-    HAVING MAX(op.count_order);
+                COUNT(order_id) AS count_order, MONTH(order_date) AS month
+            FROM 
+                Orders
+            WHERE 
+                YEAR(order_date) = year
+            GROUP BY 
+                MONTH(order_date)
+         ) AS op HAVING MAX(op.count_order);
 
-RETURN (max);
+    
+    RETURN (max);
 END
