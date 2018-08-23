215cbcc7b9ebf73fe70c4a07a73718dd300c831a
meta-bhavika-mathur <41181792+meta-bhavika-mathur@users.noreply.github.com>
2018-08-23 09:27:48 +0530

    Update Assignment3Part2.sql

--- Assignment3Part2.sql
+++ Assignment3Part2.sql
@@ -13,5 +13,27 @@ GROUP BY
 HAVING 
     COUNT(pc.product_id) > 1;
 
-# To display Count of products as per given price range..
+# To display Count of products as per given price range.
 
+SELECT 
+    pr.priceRange, COUNT(*) AS ProductCount
+FROM
+    (SELECT 
+        CASE
+                WHEN product_price > 0 AND product_price <= 100 THEN '0-100'
+                WHEN product_price > 100 AND product_price <= 500 THEN '100-500'
+                ELSE 'Above 500'
+            END AS priceRange
+    FROM
+        Product) AS pr
+GROUP BY pr.priceRange;
+
+# To display categories along with number of products under each category
+
+SELECT 
+    c.category_name, COUNT(*) AS NumberOfProducts
+FROM 
+    Category AS c
+LEFT JOIN 
+    ProductCategory AS pc ON c.category_id = pc.category_id 
+GROUP BY pc.category_id;
